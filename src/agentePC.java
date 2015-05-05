ef06967944ae3a87f96a47b8eba29da28f018cd5
Fernando <Fernando>
2015-05-05 11:58:21 -0300

    Deu certo a sincronização

--- src/agentePC.java
+++ src/agentePC.java
@@ -102,7 +102,6 @@ public class agentePC extends Agent { /**
 						
 //						agenteApcBD.getChild("estado").setText("aberta");
 						
-<<<<<<< HEAD
 //						String cr =  agenteApcBD.getChild("cr").getText(); //Dá no mesmo que a seguinte linha de código
 						String cr =  agenteApcBD.getChildText("cr");
 						System.out.println("O estado do PCC é "+cr+"." ); //Só pra testar se tá dando certo
@@ -262,9 +261,6 @@ public class agentePC extends Agent { /**
 										}// fim do handleAllResponses do comportamento Contract net
 									}// fim do if para ver se 
 		*/					}// fim do if(deltaP<0) 
-=======
-
->>>>>>> sincronizar
 					}else if(filtro_Inform.getContent().equals("1")){
 						System.out.println("Chave está fechada!!!!!!!");
 					}else{
@@ -272,166 +268,13 @@ public class agentePC extends Agent { /**
 					}
 					
 					
-//					}// fim do 	if get.content = 0
+					}// fim do 	if get.content = 0
 					
 				}// fim o if para saber se inform != null
 			} // fim do onTick 
 		}); //fim do comportamento temporal TickerBehaviour
 		
-		addBehaviour(new TickerBehaviour(this,100){
-			public void onTick(){
-//				String cr =  agenteApcBD.getChild("cr").getText(); //Dá no mesmo que a seguinte linha de código
-				String cr =  agenteApcBD.getChildText("cr");
-				System.out.println("O estado do PCC é "+cr+"." ); //Só pra testar se tá dando certo
-				
-				if (cr.equals("0")){
-//					System.out.println("A microrrede está desconectada!");
-					
-					/********************************************************************************
-					 * FIPA Subscribe Initiator para saber o valor de Potência do DER e Carga
-					 ********************************************************************************/	
-					ACLMessage msgColetarPot = new ACLMessage(ACLMessage.SUBSCRIBE); // Campo da mensagem SUBSCRIBE
-					msgColetarPot.setProtocol(FIPANames.InteractionProtocol.FIPA_SUBSCRIBE);
-					msgColetarPot.setContent("Potencia");
-					
-					/**
-					 * Aqui vamos acessar o XML do APC para pesquisar o nome dos agentes de geração intermitente
-					 * e cargas para calcular o balanço de potência na microrrede
-					 * */
-					List lista = agenteApcBD.getChild("agentesGeracaoNaoControladas").getChildren(); 
-//					System.out.println("O nome dos AGs intermitentes são: "+lista);
-					Iterator i = lista.iterator();
-					
-				    while(i.hasNext()) {
-				    	Element elemento = (Element) i.next();
-
-//				    	String nome = String.valueOf(elemento.getText());
-//				    	String nome = String.valueOf(elemento.getChild();
-				    	String nome = String.valueOf(elemento.getName());
-				    	System.out.println("O nome é: "+nome);
-				    	if (nome!= null && nome.length()>0 && nome!= "nenhum"){
-							System.out.println("Entrou no ifl!!!!!");
-				    		//quantChaves();
-//				    		cont1 = cont1 + 1;
-				    		System.out.println("-"+getLocalName()+": Solicitando valor de carga a "+nome);
-				    		msgColetarPot.addReceiver(new AID((String) nome, AID.ISLOCALNAME));
-				    		//System.out.println("o valor atual de cont1 �: "+cont1);
-				    	}
-				    }
-				    
-				    addBehaviour(new SubscriptionInitiator(myAgent,msgColetarPot){
-				   
-						private static final long serialVersionUID = 1L;
-
-						protected void handleAgree(ACLMessage agree){
-				    		
-				    	}//Fim do handleAgree do Subscribe
-						
-						protected void handleRefuse(ACLMessage refuse) {
-							//Se recusar
-						}
-						protected void handleFailure(ACLMessage failure) {
-							//Erro
-						}
-				    }); // Fim do comportamento FIPA Subscribe -> addBehaviour(new SubscriptionInitiator(myAgent,msgColetarPot){
-				    //Obs.: Por enquanto vou colocar valores aleatórios para cacular o balanço de potência na microrrede
-				    // Mas ai tenho que já ter uma base da potência da microrrede, das cargas...
-					
-					/**
-					 * Aqui supõe-se que eu já enviei um subscribe para o AG intermitente e cargas
-					 * e tenho o valor do balanço de potência na microrrede (deltaP)
-					 */
-					double deltaP = -1000; //Balanço de potência na microrrede. 
-//					System.out.println("O valor do balanço é "+deltaP); //Só pra testar
-
-					/**
-					 * Um valor positivo de deltaP diz que tá sobrando energia. Um valor negativo, diz que tá com déficit, ou seja, 
-					 * não tem geração suficiente.
-					 * Se deltaP positivo tá tudo ok. 
-					 * Se deltaP negativo, então verifica-se se tem dispositivos armazenadores de energia. 
-					 *  */
-					if(deltaP<0){ 
-						/**se entrar nesse if, será necessário consultar o XMl para averiguar se há dispositivos
-						 * armazenadores de energia
-						 */
-//						List lista = agenteChaveBD.getChild("sentido").getChild("sentido2").getChild("outrasChaves").getChildren(); 
-//
-//						Iterator i = lista.iterator();
-//
-//						while(i.hasNext()) {
-//							Element elemento = (Element) i.next();
-//							String nome = String.valueOf(elemento.getText());
-//							if (nome!= null && nome.length()>0 && nome!= "nenhum"){
-//								
-//								cont1 = cont1 + 1;
-//								System.out.println("As outras chaves s�o: "+nome);
-//								msg.addReceiver(new AID((String) nome, AID.ISLOCALNAME));
-//
-//							}
-//						}// fim do while(i.hasNext())
-						
-//						msg.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
-//						msg.setContent("abrir");
-						
-						/**
-						 * Se tiver dispositivos armazenadores, manda-se um contracte net para todos ao mesmo tempo 
-						 * solicitando um deltaP. Devo pegar um pouco de cada ou pego o de uma vez só? Como sei que só terá 
-						 * um banco de baterias vou fazer pegando só de um dispositivo de armazenamento logo.
-						 */
-						/*if(){   //Se tem dispositivos armazenadores de energia, então
-						*//*********************************************************************************
-						 * FIPA Contract Net Initiator para negociação deltaP com sistemas de armazenamento de energia
-						 *********************************************************************************//*
-							ACLMessage negociar = new ACLMessage(ACLMessage.CFP);
-							List lista1 = agenteAlimentadorBD.getChild("outrosALs").getChildren(); 
-							Iterator i = lista1.iterator();
-	
-	
-							while(i.hasNext()) {
-	
-								Element elemento = (Element) i.next();
-	
-								//String nome = String.valueOf(elemento.getText());
-								String nome = String.valueOf(elemento.getText());
-								System.out.println("-<<"+agenteAlimentador+">>: o outro AL � "+nome);
-								//negociar.addReceiver(new AID((String) nome, AID.ISLOCALNAME));
-								negociar.addReceiver(new AID( nome, AID.ISLOCALNAME));
-	
-							}
-	
-							// String carga = agenteAlimentadorBD.getChild("cargaPerdida").getAttributeValue("valor");
-							negociar.setProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
-							negociar.setContent(carga); //Carga perdida
-	
-							addBehaviour(new ContractNetInitiator(myAgent, negociar) {
-	
-								protected void handlePropose(ACLMessage propose, Vector v) {
-									System.out.println("-<<"+agenteAlimentador+">>: o agente "+propose.getSender().getLocalName()+" disse "+propose.getContent());
-								}
-	
-								protected void handleRefuse(ACLMessage refuse) {
-									System.out.println("-<<"+agenteAlimentador+">>: o agente "+refuse.getSender().getLocalName()+" recusou");
-								}
-	
-								protected void handleFailure(ACLMessage failure) {
-									if (failure.getSender().equals(myAgent.getAMS())) {
-	
-										System.out.println("N�o existe outros agentes ALs");
-									}
-									else {
-										System.out.println("-<<"+agenteAlimentador+">>: o agente "+failure.getSender().getLocalName()+" falhou");
-									}
-									// Immediate failure --> we will not receive a response from this agent
-								}
-	
-								protected void handleAllResponses(Vector responses, Vector acceptances) {
-	
-								}// fim do handleAllResponses do comportamento Contract net
-							}// fim do if para ver se 
-*/					}// fim do if(deltaP<0) 
-				}
-			}
-		});
+		
 		
 
 					
